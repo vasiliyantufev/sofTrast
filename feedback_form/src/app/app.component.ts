5487e5effa7364057e9677c78d4fbd7a99e8be72
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2024-11-06 23:22:51 +0300

    feedback component init

--- feedback_form/src/app/app.component.ts
+++ feedback_form/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FeedbackComponent } from './feedback/feedback.component'; // Убедитесь, что путь правильный
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FeedbackComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
