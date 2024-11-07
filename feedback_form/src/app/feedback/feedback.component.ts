d5e3a328e809b1fccf905d6250f6c6417e7b9951
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2024-11-07 15:39:33 +0300

    feedback html

--- feedback_form/src/app/feedback/feedback.component.ts
+++ feedback_form/src/app/feedback/feedback.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  // Импортируем CommonModule для использования директивы *ngIf
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.css'
 })
