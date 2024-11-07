81cfb44baf2968b52d41cdbcd6ef05563f1c0789
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2024-11-07 17:16:58 +0300

    feedback js

--- feedback_form/src/app/feedback/feedback.component.ts
+++ feedback_form/src/app/feedback/feedback.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';  // Импортируем CommonModule для использования директивы *ngIf
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './feedback.component.html',
-  styleUrl: './feedback.component.css'
+  styleUrls: ['./feedback.component.css'],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule]
 })
-export class FeedbackComponent {
+export class FeedbackComponent implements OnInit {
+  feedbackForm: FormGroup;
+  successMessageVisible: boolean = false;
+  errorMessageVisible: boolean = false;
 
+  constructor(private fb: FormBuilder) {
+    this.feedbackForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.feedbackForm.valid) {
+      const feedbackData = this.feedbackForm.value;
+
+      // Здесь можно сделать HTTP-запрос на сервер
+      console.log('Feedback submitted:', feedbackData);
+      
+      // Если отправка прошла успешно
+      this.successMessageVisible = true;
+      this.errorMessageVisible = false;
+
+      // Сброс формы после отправки
+      this.feedbackForm.reset();
+    } else {
+      this.errorMessageVisible = true;
+      this.successMessageVisible = false;
+    }
+  }
 }
