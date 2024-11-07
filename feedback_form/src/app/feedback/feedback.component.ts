afbbe461c75a82659ae7a78a595710e007247bec
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2024-11-08 01:11:31 +0300

    capcha

--- feedback_form/src/app/feedback/feedback.component.ts
+++ feedback_form/src/app/feedback/feedback.component.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http'; // Импортируем HttpClient
 import { Observable } from 'rxjs'; // Импортируем Observable (опционально)
+import { RecaptchaModule } from 'ng-recaptcha';  // Импортируем RecaptchaModule
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.css'],
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule]
+  imports: [CommonModule, ReactiveFormsModule, RecaptchaModule]
 })
 export class FeedbackComponent implements OnInit {
   feedbackForm: FormGroup;
@@ -24,11 +25,24 @@ export class FeedbackComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required, Validators.pattern(/^\+?[0-9\s\(\)-]{7,15}$/)]], // Валидация для номера телефона
       subject: ['support', Validators.required],
-      message: ['', Validators.required],
+      message: ['', Validators.required],      
+      recaptcha: ['', Validators.required] // Поле для токена reCAPTCHA
     });
   }
 
   ngOnInit(): void {}
+
+  resolved(captchaResponse: string | null): void {
+    // Проверяем, что значение не null
+    if (captchaResponse) {
+      this.feedbackForm.controls['recaptcha'].setValue(captchaResponse);
+    } else {
+      // Если капча не была решена, можно очистить или сбросить значение
+      this.feedbackForm.controls['recaptcha'].setValue(null);
+    }
+  }
+  
+
 
   onSubmit(): void {
     if (this.feedbackForm.valid) {
